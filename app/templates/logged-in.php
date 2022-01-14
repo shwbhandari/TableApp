8d0a4ce0284d320a1dffefb9e830220aa981807a
shwbhandari <shwjain89@gmail.com>
2022-01-14 10:26:14 +0530

    insert table logged-in version

--- app/templates/logged-in.php
+++ app/templates/logged-in.php
@@ -1,3 +1,6 @@
+<?php 
+// header('location:http://localhost/create.php'); 
+?>
 <?php $this->layout('_layout'); ?>
 
 <?php $this->start('hero') ?>
@@ -27,7 +30,7 @@
     <div class="mb-16 w-full lg:grid lg:grid-cols-12 lg:gap-x-5">
         <aside class="py-6 px-2 sm:px-6 lg:py-0 lg:px-0 lg:col-span-3">
             <nav class="space-y-1">
-                <!-- <a href="#" class="bg-gray-50 text-indigo-700 hover:text-indigo-700 hover:bg-white group rounded-md px-3 py-2 flex items-center text-sm font-medium" aria-current="page">
+                <!-- <a href="#user" class="bg-gray-50 text-indigo-700 hover:text-indigo-700 hover:bg-white group rounded-md px-3 py-2 flex items-center text-sm font-medium" aria-current="page">
                     <svg class="text-indigo-500 group-hover:text-indigo-500 flex-shrink-0 -ml-1 mr-3 h-6 w-6" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor" aria-hidden="true">
                         <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M5.121 17.804A13.937 13.937 0 0112 16c2.5 0 4.847.655 6.879 1.804M15 10a3 3 0 11-6 0 3 3 0 016 0zm6 2a9 9 0 11-18 0 9 9 0 0118 0z" />
                     </svg>
@@ -36,7 +39,7 @@
                     </span>
                 </a> -->
 
-                <a href="#" class="text-gray-900 hover:text-gray-900 hover:bg-gray-50 group rounded-md px-3 py-2 flex items-center text-sm font-medium">
+                <a href="#create" class="text-gray-900 hover:text-gray-900 hover:bg-gray-50 group rounded-md px-3 py-2 flex items-center text-sm font-medium">
                     <svg class="text-gray-400 group-hover:text-gray-500 flex-shrink-0 -ml-1 mr-3 h-6 w-6" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor" aria-hidden="true">
                         <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M15 7a2 2 0 012 2m4 0a6 6 0 01-7.743 5.743L11 17H9v2H7v2H4a1 1 0 01-1-1v-2.586a1 1 0 01.293-.707l5.964-5.964A6 6 0 1121 9z" />
                     </svg>
@@ -45,131 +48,144 @@
                     </span>
                 </a>
 
-                <a href="#cookies" class="text-gray-900 hover:text-gray-900 hover:bg-gray-50 group rounded-md px-3 py-2 flex items-center text-sm font-medium">
+                <a href="#display" class="text-gray-900 hover:text-gray-900 hover:bg-gray-50 group rounded-md px-3 py-2 flex items-center text-sm font-medium">
                     <svg class="text-gray-400 group-hover:text-gray-500 flex-shrink-0 -ml-1 mr-3 h-6 w-6" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor" aria-hidden="true">
                         <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M11 4a2 2 0 114 0v1a1 1 0 001 1h3a1 1 0 011 1v3a1 1 0 01-1 1h-1a2 2 0 100 4h1a1 1 0 011 1v3a1 1 0 01-1 1h-3a1 1 0 01-1-1v-1a2 2 0 10-4 0v1a1 1 0 01-1 1H7a1 1 0 01-1-1v-3a1 1 0 00-1-1H4a2 2 0 110-4h1a1 1 0 001-1V7a1 1 0 011-1h3a1 1 0 001-1V4z" />
                     </svg>
                     <span class="truncate">
-                        Show Tables
+                        Display Tables
                     </span>
                 </a>
             </nav>
         </aside>
 
         <div class="space-y-6 sm:px-6 lg:px-0 lg:col-span-9">
-            <!-- <div id="user">
-                <div class="shadow sm:rounded-md sm:overflow-hidden">
-                    <div class="bg-white">
-                        <div class="px-4 py-5 sm:px-6 bg-gray-100">
-                            <h3 class="text-lg leading-6 font-medium text-gray-900">User Information</h3>
-                        </div>
+         
+        
+<?php
+  $server = "localhost"; 
+  $username = "root";
+  $password = "";
+  $database="diy";
+  $conn = mysqli_connect($server, $username, $password ,$database); 
+  $mysqli = new mysqli($server, $username, $password ,$database);
 
-                        <div class="px-4 py-5 sm:px-6 grid grid-cols-6 gap-6">
-                            <?php foreach($session->user as $key => $data): ?>
-                                <?php $row = uniqid(); ?>
-
-                                <?php if(is_string($data)): ?>
-                                    <div class="col-span-6">
-                                        <label for="<?php echo $row ?>" class="block text-sm font-medium text-gray-700"><?php echo $key ?></label>
-                                        <input value="<?php echo htmlspecialchars($data) ?>" type="text" readonly="true" name="<?php echo $row ?>" id="<?php echo $row ?>" class="mt-1 block w-full bg-white border border-gray-300 rounded-md shadow-sm py-2 px-3 focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm">
-                                    </div>
-                                <?php endif ?>                             
-                            <?php endforeach ?>
-                        </div>
-                    </div>
-                </div>
-            </div> -->
-
-            <div class="text-center "style="font-size: xxx-large">
-          <strong> Create table</strong> 
-             </div>
-          <?php
-
-          $self = $_SERVER['PHP_SELF'];
-$fields = null;
-$db = null;
-
-if(isset($_POST['field_submit'])) {
-  $fields = $_POST['fields'];
-}
-else if(isset($_POST['db_submit'])) {
-  $db =   "diy";
-  $name =   $_POST['name'];
-  $table =  $_POST['table'];
-  $type =   $_POST['type'];
-  $size =   $_POST['size'];
-}
-
-if( !$fields and !$db )
-{ $form="<div class=\"text-center\">";
-  $form .="<form action=\"$self\" method=\"post\">";
-  $form.="How many fields are needed in the new table?<br>";
-  $form.="<input type=\"text\" name=\"fields\" size=\"5\"  style=\"  border: 2px solid black\" >";
-  $form.="<br> <br>";
-  $form.="<input type=\"submit\" name=\"field_submit\"  style=\" border: 2px solid black;border-radius:9px;font-size:1rem\"  value=\"Submit\">";
-  echo($form);
-}
-else if( !$db )
-{ $form="<div class=\"text-center\">";
-  $form .="<form action=\"$self\" method=\"post\">";
-//   $form.="Database:     <input type=\"text\" name=\"db\"><br>";
-
-  $form.=" Enter Table Name:  <input type=\"text\"  required=\"required\" name=\"table\" size=\"10\" style=\"  border: 2px solid black\"><br><br> ";
-  for ($i = 0 ; $i <$fields; $i++) 
-  {   
-    $form.="Column Name:<input type=\"text\"  required=\"required\" name=\"name[$i]\" size=\"10\" style=\"  border: 2px solid black\"> ";
-    $form.="Type: <select name=\"type[$i]\"style=\"  border: 2px solid black\">";
-    $form.="<option value=\"char\">char</option>";	
-    $form.="<option value=\"varchar\">varchar</option>";
-    $form.="<option value=\"int\">int</option>";
-    $form.="<option value=\"float\">float</option>";
-    $form.="<option value=\"timestamp\">timestamp</option>";
-    $form.="</select> ";
-    $form.="Size:<input type=\"text\" name=\"size[$i]\" size=\"5\" style=\"  border: 2px solid black\"> <br><br>";
-  }
-  $form.="<input type=\"submit\" name=\"db_submit\"  style=\" border: 2px solid black;border-radius:9px;font-size:1rem\"  value=\"Submit\">";
+ if (isset($_POST["createTable"])) 
+ { 
+    $tname= $_POST['tname'];
+    $type=$_POST['type'];
+    $colname=$_POST['colname'];
+    $size=$_POST['size'];
  
-  echo($form);
+  if(! $conn ) 
+  {
+       die('Could not connect: ' . mysqli_connect_error($conn));
+  } 
+ if(($type=="char") or ($type=="varchar")){
+ $sql ="CREATE TABLE $tname ($colname $type($size))";
 }
 else
-{ 
-     $conn = mysqli_connect("localhost","root", "" ,$db); 
-  $mysqli = new mysqli("localhost","root", "" ,$db);
-  $num_columns = count($name);
+{
+    $sql = "CREATE TABLE $tname ("."$colname $type)";
+}
 
-  $sql = "create table $table (";
-  for ($i = 0; $i < $num_columns; $i++) 
-  {
-    $sql .= "$name[$i] $type[$i]";
-    if(($type[$i] =="char") or ($type[$i] =="varchar"))
-    {
-      if($size[$i] !="" ){ $sql.= "($size[$i])"; }
-    }
-    if(($i+1) != $num_columns){ $sql.=","; }
-  }
-  $sql .= ")";
-
-
-
-  $mysqli -> select_db("$db");
+$mysqli -> select_db("diy");
 
  $retval = $mysqli -> query( $sql ); 
  if(! $retval ) 
- {  echo"<div class=\"text-center\" style=\"color: red\">";
-   
-   
-  echo("Can't create " . $mysqli -> error);
-  echo"</div>";
+ {
+  echo("Cant create " . $mysqli -> error);
+ 
  } 
  else
-
- { echo"<div class=\"text-center\" style=\"color: green\">";
-    echo "Table created successfully\n";
-    echo"</div>";
+ {echo "Table created successfully\n";
  }
-  mysqli_close($conn); 
 }
-?>
+if(isset($_POST['showTable'])){
+    $tname= $_POST['tname'];
+    $tables = $this->db->list_tables();
+ 
+}
+  mysqli_close($conn); 
+  
+   ?>
+        
        
+            <div id="create">
+                <div class="shadow sm:rounded-md sm:overflow-hidden">
+                    <div class="bg-white">
+                        <div class="px-4 py-5 sm:px-6 bg-gray-100">
+                            <h3 class="text-lg leading-6 font-medium text-gray-900">Create Tables</h3>
+                        </div>
+                        <p></p>
+                        <form method="post" class="container" id="form" enctype="multipart/form-data">
+                        <div class="px-4 py-5 sm:px-6 grid grid-cols-6 gap-6">
+                                
+                                <div class="col-span-6">
+                                    <label  class="block text-sm font-medium text-gray-700">Table name</label>
+                                    <input placeholder="Table Name" type="text"  name="tname" id="tname" class="mt-1 block w-full bg-white border border-gray-300 rounded-md shadow-sm py-2 px-3 focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm">
+                                </div>
+                                
+                                <div class="col-span-6">
+                                    <label  class="block text-sm font-medium text-gray-700">Column name</label>
+                                    <input placeholder="Table Name" type="text"  name="colname" id="colname" class="mt-1 block w-full bg-white border border-gray-300 rounded-md shadow-sm py-2 px-3 focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm">
+                                </div>
+                                <div class="col-span-6">
+                                    <label  class="block text-sm font-medium text-gray-700">Select Column Type</label>
+                                    <select  name="type" class="mt-1 block w-full bg-white border border-gray-300 rounded-md shadow-sm py-2 px-3" aria-label="Default select example">
+                                    <option selected>column type</option>
+                                    <option value="varchar">string</option>
+                                    <option value="int">Number</option>
+                                    <option value="boolean">Boolean</option>
+                                    <option value="varchar" >email</option>
+                                    <option value="timestamp">Datetime</option>
+                                    </select>
+                                </div> 
+                                <div class="col-span-6">
+                                    <label  class="block text-sm font-medium text-gray-700">(enter size for string or email)</label>
+                                    <input placeholder="Size" type="text"  name="size" id="size" class="mt-1 block w-full bg-white border border-gray-300 rounded-md shadow-sm py-2 px-3 focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm">
+                                </div>          
+                                <div class="col-span-6">
+                                    <button class="button" type="submit" name="createTable" class="btn-primary" style="border-radius:5px;padding:2px;background-color:grey"><span><strong>Create</strong></span></button>
+                                </div>             
+                          
+                        </div>
+                    </div>
+                </div>
+            </div>
+
+            
+            <div id="display">
+                <div class="shadow sm:rounded-md sm:overflow-hidden">
+                    <div class="bg-white">
+                        <div class="px-4 py-5 sm:px-6 bg-gray-100">
+                            <h3 class="text-lg leading-6 font-medium text-gray-900">Display Tables</h3>
+                        </div>
+                        <form method="post" class="container" id="showform" enctype="multipart/form-data">
+                        <div class="px-4 py-5 sm:px-6 grid grid-cols-6 gap-6">
+                        <div class="col-span-6">
+                                    <label  class="block text-sm font-medium text-gray-700">Table name</label>
+                                    <select name="tname" class="mt-1 block w-full bg-white border border-gray-300 rounded-md shadow-sm py-2 px-3" aria-label="Default select example">
+                        
+                        </div>
+                                
+                                <div class="col-span-6">
+                                    <label  class="block text-sm font-medium text-gray-700">Column name</label>
+                                    <input placeholder="Table Name" type="text"  name="colname" id="colname" class="mt-1 block w-full bg-white border border-gray-300 rounded-md shadow-sm py-2 px-3 focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm">
+                                </div>
+                                        
+                                <div class="col-span-6">
+                                    <button class="button" type="submit" name="submit" class="btn-primary" style="border-radius:5px;padding:2px;background-color:grey"><span><strong>Create</strong></span></button>
+                                </div>             
+                          
+                        </div>
+                    </div>
+                </div>
+            </div>
+
+        </div>
+
     </div>
+    
 <?php $this->stop() ?>
+
