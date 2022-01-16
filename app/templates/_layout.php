d38be56cc882ca169299efd0f66655e8cca3055f
shweta jain <42119665+shwbhandari@users.noreply.github.com>
2022-01-16 20:04:33 +0530

    Update _layout.php

--- app/templates/_layout.php
+++ app/templates/_layout.php
@@ -5,6 +5,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0" />
     <link href="//unpkg.com/tailwindcss@^2/dist/tailwind.min.css" rel="stylesheet">
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css" integrity="sha384-MCw98/SFnGE8fJT3GXwEOngsV7Zt27NXFoaoApmYm81iuXoPkFOJwJ8ERdknLPMO" crossorigin="anonymous">
+    <link rel="stylesheet" type="text/css" href="https://cdn.datatables.net/1.11.3/css/jquery.dataTables.min.css"/>
     <title>Table App</title>
 
     <style>
@@ -18,6 +19,9 @@
 
         textarea {
             min-height: 15em;
+        }
+        a:hover{
+            text-decoration: none; 
         }
     </style>
 </head>
@@ -74,9 +78,27 @@
         </div>
     </main>
 
-    <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
+<script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
 <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.3/umd/popper.min.js" integrity="sha384-ZMP7rVo3mIykV+2+9J3UJ46jBk0WLaUAdn689aCwoqbBJiSnjAK/l8WvCWPIPm49" crossorigin="anonymous"></script>
 <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/js/bootstrap.min.js" integrity="sha384-ChfqqxuZUCnJSK3+MXmPNIyE6ZbWh2IMqE241rYiqJxyMiZ6OW/JmZQ5stwEULTy" crossorigin="anonymous"></script>
+
+<script type="text/javascript" src="https://cdn.datatables.net/1.11.3/js/jquery.dataTables.min.js"></script>
+<script>
+$(document).ready(function () {
+          $('#mydatatable').DataTable({
+          "bPaginate": true,
+  "bLengthChange": true,
+  "bFilter": true,
+  "bSort": false,
+  "bInfo": true,
+  "bAutoWidth": false,
+  "dom": 'Bfrtip',
+  "buttons": [
+    'copy', 'csv', 'excel', 'pdf', 'print'
+  ]});
+      });
+      
+</script>
 </body>
 
 </html>
